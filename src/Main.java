52ae955dc580d81c1ba68f5173dc97b2079e82a9
Z0d1akAllSeeingEye <andriirudyk32@gmail.com>
2024-10-08 09:51:14 +0200

    Add

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
         Adder adder = new Adder();
         System.out.println(adder.add(1, 2));
         Subtractor subtractor = new Subtractor();
-
         System.out.println(subtractor.subtract(6, 3));
     }
 }
